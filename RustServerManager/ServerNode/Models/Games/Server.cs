33bd9c65a844c7636509d77295c5cd647bdfee8a
Adam Woodhead <github@adamwoodhead.co.uk>
2020-05-26 16:59:20 +0100

    init abstract server class

---
+++ RustServerManager/ServerNode/Models/Games/Server.cs
@@ -0,0 +1,63 @@
+﻿using Pty.Net;
+using ServerNode.Models.Steam;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerNode.Models.Games
+{
+    internal abstract class Server
+    {
+        int ID { get; set; }
+
+        string IPAddress { get; set; }
+
+        string Port { get; set; }
+
+        string SafeStopCommand { get; set; }
+
+        string WorkingDirectory { get; set; }
+
+        int? SteamDBID { get; set; }
+
+        IPtyConnection PseudoTerminal { get; set; }
+
+        IPtyConnection Commandline { get; }
+
+        bool Install()
+        {
+            throw new NotImplementedException();
+        }
+
+        void Kill()
+        {
+            this.PseudoTerminal.Kill();
+        }
+
+        void Reinstall()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool Restart()
+        {
+            Stop();
+            Start();
+        }
+
+        bool Start()
+        {
+
+        }
+
+        bool Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool Uninstall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
