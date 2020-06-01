75ebeb307ce8401fcba5715e741584e3dca27e50
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-01 11:54:07 +0100

    commented steamapp.cs

--- RustServerManager/ServerNode/Models/Steam/SteamApp.cs
+++ RustServerManager/ServerNode/Models/Steam/SteamApp.cs
@@ -6,12 +6,39 @@ namespace ServerNode.Models.Steam
 {
     internal class SteamApp
     {
+        /// <summary>
+        /// App Name
+        /// </summary>
         internal string Name { get; }
+        
+        /// <summary>
+        /// App Short Name
+        /// </summary>
         internal string ShortName { get; }
+
+        /// <summary>
+        /// Windows Relative Executable Path
+        /// </summary>
         private string WindowsRelativeExecutablePath { get; }
+
+        /// <summary>
+        /// Linux Relative Executable Path
+        /// </summary>
         private string LinuxRelativeExecutablePath { get; }
+
+        /// <summary>
+        /// Native Executable Path
+        /// </summary>
         internal string RelativeExecutablePath { get => Utility.OperatingSystemHelper.IsWindows() ? WindowsRelativeExecutablePath : LinuxRelativeExecutablePath; }
+        
+        /// <summary>
+        /// Apps Steam DB ID
+        /// </summary>
         internal int SteamID { get; }
+
+        /// <summary>
+        /// Default Commandline for Server
+        /// </summary>
         public string[] DefaultCommandLine { get; }
 
         internal SteamApp(string name, string shortName, string relativeWindowsExecutablePath, string relativeLinuxExecutablePath, int steamID, string[] defaultCommandLine)
