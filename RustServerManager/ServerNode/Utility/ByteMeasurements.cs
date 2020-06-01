b05127d076f08fb4ab36fb637bb6850d7aca3a13
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-01 11:54:21 +0100

    commented bytemeasurements.cs

--- RustServerManager/ServerNode/Utility/ByteMeasurements.cs
+++ RustServerManager/ServerNode/Utility/ByteMeasurements.cs
@@ -6,11 +6,21 @@ namespace ServerNode.Utility
 {
     internal static class ByteMeasurements
     {
+        /// <summary>
+        /// Convert Bytes To KiloBytes
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         internal static double BytesToKB(double bytes)
         {
             return bytes / 1024;
         }
 
+        /// <summary>
+        /// Convert Bytes To MegaBytes
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         internal static double BytesToMB(double bytes)
         {
             return BytesToKB(bytes) / 1024;
