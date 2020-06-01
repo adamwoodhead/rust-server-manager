73389769c8a8c39ba74a138839c42d9a28c84440
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-01 11:54:31 +0100

    commented oshelper.cs

--- RustServerManager/ServerNode/Utility/OSHelper.cs
+++ RustServerManager/ServerNode/Utility/OSHelper.cs
@@ -10,12 +10,28 @@ namespace ServerNode.Utility
         private static bool? isLinux = null;
         private static bool? isFreeBSD = null;
 
+        /// <summary>
+        /// Is the current operating system Windows
+        /// </summary>
+        /// <returns></returns>
         public static bool IsWindows() => isWindows ??= RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
+        /// <summary>
+        /// Is the current operating system MacOS
+        /// </summary>
+        /// <returns></returns>
         public static bool IsMacOS() => isMacOS ??= RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
 
+        /// <summary>
+        /// Is the current operating system Linux
+        /// </summary>
+        /// <returns></returns>
         public static bool IsLinux() => isLinux ??= RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
+        /// <summary>
+        /// Is the current operating system FreeBSD
+        /// </summary>
+        /// <returns></returns>
         public static bool IsFreeBSD() => isFreeBSD ??= RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
     }
 }
