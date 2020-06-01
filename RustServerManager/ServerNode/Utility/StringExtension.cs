ad098b85fcad2786e43b78642d5d35b8308954d6
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-01 11:54:42 +0100

    commented stringextension.cs

--- RustServerManager/ServerNode/Utility/StringExtension.cs
+++ RustServerManager/ServerNode/Utility/StringExtension.cs
@@ -7,8 +7,20 @@ namespace ServerNode.Utility
 {
     internal static class StringExtension
     {
+        /// <summary>
+        /// Checks if the string ends with any of the states strings
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needles"></param>
+        /// <returns></returns>
         internal static bool EndsWithAny(this string haystack, IEnumerable<string> needles) => EndsWithAny(haystack, needles.ToArray());
 
+        /// <summary>
+        /// Checks if the string ends with any of the states strings
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needles"></param>
+        /// <returns></returns>
         internal static bool EndsWithAny(this string haystack, params string[] needles)
         {
             foreach (string needle in needles)
@@ -22,8 +34,20 @@ namespace ServerNode.Utility
             return false;
         }
 
+        /// <summary>
+        /// Checks if the string contains any of the states strings
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needles"></param>
+        /// <returns></returns>
         internal static bool ContainsAny(this string haystack, IEnumerable<string> needles) => ContainsAny(haystack, needles.ToArray());
 
+        /// <summary>
+        /// Checks if the string contains any of the states strings
+        /// </summary>
+        /// <param name="haystack"></param>
+        /// <param name="needles"></param>
+        /// <returns></returns>
         internal static bool ContainsAny(this string haystack, params string[] needles)
         {
             foreach (string needle in needles)
