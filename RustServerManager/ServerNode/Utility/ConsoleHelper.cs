ed7c3091f8ee75f4dbbbd80005b0560a5bbafea2
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-05 13:33:45 +0100

    added ability to stall command input and hijack the readline for input elsewhere, also added switch to redact the inputted string

---
+++ RustServerManager/ServerNode/Utility/ConsoleHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerNode.Utility
+{
+    internal static class ConsoleHelper
+    {
+        internal static async Task<string> InteruptReadForInput(bool redact = false, string redactText = "################ <REDACTED> ################")
+        {
+            TaskCompletionSource<string> interuptInput = new TaskCompletionSource<string>();
+            Program.InteruptedInput = interuptInput;
+            string input = await Program.InteruptedInput.Task;
+
+            if (redact)
+            {
+                //Move cursor to just before the input just entered
+                int top = Console.CursorTop;
+                Console.CursorTop -= 1;
+                Console.CursorLeft = 0;
+                //blank out the content that was just entered
+                Console.WriteLine(redactText.PadRight(Console.BufferWidth - redactText.Length));
+                //move the cursor to just before the input was just entered
+                Console.CursorTop = top;
+                Console.CursorLeft = 0;
+            }
+
+            return input;
+        }
+    }
+}
