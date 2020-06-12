bedd6e0f7477913f092ef04a4c705821ee9d299b
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-12 13:19:06 +0100

    linux port manager

--- RustServerManager/ServerNode/Native/Linux/PortManager.cs
+++ RustServerManager/ServerNode/Native/Linux/PortManager.cs
@@ -1,5 +1,6 @@
 ﻿using ServerNode.Models.Servers;
 using System;
+using System.Linq;
 
 namespace ServerNode.Native.Linux
 {
@@ -8,13 +9,41 @@ namespace ServerNode.Native.Linux
         // TODO Linux Add Firewall Rule
         internal static bool AddFirewallRule(Server server)
         {
-            throw new NotImplementedException();
+            string[] output1 = SH.Shell(Program.WorkingDirectory, $"-c \"iptables -A INPUT -p udp --dport {server.Port} -j ACCEPT\"");
+
+            if (output1.Count() > 0 && output1.FirstOrDefault().Contains("Permission denied."))
+            {
+                return false;
+            }
+
+            string[] output2 = SH.Shell(Program.WorkingDirectory, $"-c \"iptables -A INPUT -p tcp --dport {server.Port} -j ACCEPT\"");
+
+            if (output2.Count() > 0 && output2.FirstOrDefault().Contains("Permission denied."))
+            {
+                return false;
+            }
+
+            return true;
         }
 
         // TODO Linux Remove Firewall Rule
         internal static bool RemoveFirewallRule(Server server)
         {
-            throw new NotImplementedException();
+            string[] output1 = SH.Shell(Program.WorkingDirectory, $"-c \"iptables -D INPUT -p udp --dport {server.Port} -j ACCEPT\"");
+
+            if (output1.Count() > 0 && output1.FirstOrDefault().Contains("Permission denied."))
+            {
+                return false;
+            }
+
+            string[] output2 = SH.Shell(Program.WorkingDirectory, $"-c \"iptables -D INPUT -p tcp --dport {server.Port} -j ACCEPT\"");
+
+            if (output2.Count() > 0 && output2.FirstOrDefault().Contains("Permission denied."))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
