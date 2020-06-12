1837037532bc004b2c00c90475677691db7bd7bd
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-12 13:18:42 +0100

    linux filesystem helper, chmod/chown

---
+++ RustServerManager/ServerNode/Native/Linux/FileSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerNode.Native.Linux
+{
+    internal static class FileSystem
+    {
+        public static void Chmod(string perms, string path, bool recursive = false)
+        {
+            Linux.SH.Shell(Program.WorkingDirectory, $"-c \"chmod{(recursive ? " -R" : "")} {perms} {path}\"");
+        }
+
+        public static void Chown(string user, string directory, bool recursive = false)
+        {
+            Linux.SH.Shell(Program.WorkingDirectory, $"-c \"chown{(recursive ? " -R" : "")} {user} {directory}\"");
+        }
+    }
+}
