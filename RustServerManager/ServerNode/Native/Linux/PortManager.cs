897580796c9b9e8ecabadc994da3b1c7cf6138ea
Adam Woodhead <github@adamwoodhead.co.uk>
2020-06-12 09:59:07 +0100

    linux firewall rules TODO markers

--- RustServerManager/ServerNode/Native/Linux/PortManager.cs
+++ RustServerManager/ServerNode/Native/Linux/PortManager.cs
@@ -5,11 +5,13 @@ namespace ServerNode.Native.Linux
 {
     internal class PortManager
     {
+        // TODO Linux Add Firewall Rule
         internal static bool AddFirewallRule(Server server)
         {
             throw new NotImplementedException();
         }
 
+        // TODO Linux Remove Firewall Rule
         internal static bool RemoveFirewallRule(Server server)
         {
             throw new NotImplementedException();
